0f63a1766bd8391236621f197e84f86781e372a2
Jeremy Mark <jeremymark@Christinas-MacBook-Pro.local>
2019-10-08 21:48:30 -0500

    project update

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -17,28 +17,24 @@ class Chart extends Component {
 
         this.props.getViews()
             .then(() => {
-                // this.setState({ chartData: {...this.state.chartData, datasets: {...this.state.datasets, data: this.props.data}} })
                 const {views} = this.props;
                 let labels = [];
                 let chartData = [];
                 let backgroundColor = [];
                 let borderColor = [];
-        
-                //Add to labels array
-                // addLabel = () => {
-        
+                
                 // Set labels and data
                 for(let i = 0; i < views.length; i++) {
                     labels.push(views[i].name)
                     chartData.push(views[i].count)
                 }
-        
+                
                 const chartColors = this.getRandomNum(views.length);
-        
+                
                 // Set backgroundColor and borderColor
                 backgroundColor = chartColors[0];
                 borderColor = chartColors[1];
-        
+                
                 // Set state with our labels, data, backgroundColor, and borderColor
                 this.setState({
                     chartData: {
@@ -51,17 +47,6 @@ class Chart extends Component {
                         }]
                     }
                 })
-        
-                // }
-                // Add to data array
-                // addData = i => {
-        
-                //     datasets[0].push(this.props.data[i].count);
-                // }
-                // // Add to background color
-                // addColor 
-
-
             })
     }
 
@@ -69,33 +54,33 @@ class Chart extends Component {
         let bgColorArr = [];
         let borderColorArr = [];
         
-      
         for(let i = 0; i < numOfElements; i++) {
-          const min = 0, max = 255;
-          let spot1 = Math.floor(Math.random() * (max - min + 1) + min);
-          let spot2 = Math.floor(Math.random() * (max - min + 1) + min);
-          let spot3 = Math.floor(Math.random() * (max - min + 1) + min);
-          
-          bgColorArr.push(`rgba(${spot1}, ${spot2}, ${spot3}, .5)`)
-          borderColorArr.push(`rgba(${spot1}, ${spot2}, ${spot3}, 1)`)
-        }
-      
+            const min = 0, max = 255;
+            let spot1 = Math.floor(Math.random() * (max - min + 1) + min);
+            let spot2 = Math.floor(Math.random() * (max - min + 1) + min);
+            let spot3 = Math.floor(Math.random() * (max - min + 1) + min);
+        
+            bgColorArr.push(`rgba(${spot1}, ${spot2}, ${spot3}, .8)`)
+            borderColorArr.push(`rgba(${spot1}, ${spot2}, ${spot3}, 1)`)
+        }    
         return [bgColorArr, borderColorArr]
-      }
+    }
+
+    resetMyViews = () => {
+        this.props.resetViews();
+    }
 
     render() {
-        // console.log(this.props.data[0].count)
-        // console.log(this.state)
         return (
             <div className="chart">
-                <h3>Chart Component</h3>
+                <h3>Your Topics most studied by view count</h3>
                 <Doughnut
                     data={this.state.chartData} 
                 />
+                <button onClick={this.resetMyViews}>Reset</button>
             </div>
         )
     }
-    
 }
 
 const mapStateToProps = reduxState => {
@@ -110,17 +95,3 @@ export default connect(mapStateToProps,
         getViews
     }
 )(Chart);
-
-//                    
-// width={300}
-// height={150}
-// options={{ 
-//     maintainAspectRatio: false,
-//     title: {
-//         display: this.props.displayTitle,
-//         text: 'Your Topics most studied',
-//         fontSize: 25,
-//         legend: {
-//             display: this.props.displayLegend,
-//             position: this.props.legendPosition
-//         }
--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -10,24 +10,15 @@ export default class UserProfile extends Component {
             // chartData: {},
         }
     }
-    componentDidMount() {
-        
-    }
-
-    resetMyViews = () => {
-        this.props.resetViews();
-    }
 
     render() {
         console.log(this.props.views);
         return (
             <div>
                 My Profile                
-                <button onClick={this.resetMyViews}>Reset</button>
+                
                 <Chart />               
             </div>
         )
     }
 }
-
-// <Link to="/user">Update</Link>
